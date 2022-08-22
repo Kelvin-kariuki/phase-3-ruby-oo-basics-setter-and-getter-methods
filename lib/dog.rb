2ff63827d6252015291a6d9055b29ff19e85ca1f
kelvin-kariuki <Kariuki405@gmail.com>
2022-08-23 00:57:59 +0300

    commit tree 48509a2748f6c40cbf36215bdfc68e6af2d1cebc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+# setter method
+def name=(name)
+    # Defining class instance to global
+    @name =name
+end
+# getter method
+def name
+    @name
+end
+def breed=(breed)
+    # setter
+    @breed = breed
+end
+def breed
+    # getter
+    @breed
+end
 end
