b2a20c20b93cdf1d7284712a3107172deddf771a
kelvin-kariuki <Kariuki405@gmail.com>
2022-08-23 00:58:13 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
-end
+    # setter method
+    def name=(name)
+        # Defining class instance to global
+        @name =name
+    end
+    # getter method
+    def name
+        @name
+    end
+    def job=(job)
+        # setter
+        @job = job
+    end
+    def job
+        # getter
+        @job
+    end
+    end
